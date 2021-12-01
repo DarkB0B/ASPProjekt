be2c21195f55e43d93c4c1b4f18de376f7281d5a
miecz <miecz@DESKTOP-NE75I11>
2021-12-01 16:12:30 +0100

    Dodaj pliki projektów.

---
+++ ASPProjekt/Controllers/PostController.cs
@@ -0,0 +1,89 @@
+﻿using ASPProjekt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPProjekt.Controllers
+{
+    public class PostController : Controller
+    {
+        static List<Post> posts = new List<Post>();
+        private static int index = 0;
+        public IActionResult Index()
+        {
+            return View(posts);
+        }
+
+        public IActionResult AddForm()
+        {
+            return View();
+            
+        }
+        [HttpPost]
+
+        public IActionResult Add(Post Post)
+        {
+            if (ModelState.IsValid)
+            {
+                index++;
+                Post.Id = index;
+                posts.Add(Post);
+                Post.DateOfAdd = DateTime.Now;
+                return View("ConfirmPost");
+            }
+            else
+            {
+                return View("AddPost");
+            }
+        }
+
+        public IActionResult List()
+        {
+            return View(posts);
+        }
+
+        public IActionResult EditForm(int id)
+        {
+            return View(FindPost(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(Post editedPost)
+        {
+            if (ModelState.IsValid)
+            {
+                Post originalPost = FindPost(editedPost.Id);
+                originalPost.Title = editedPost.Title;
+                return View("List", posts);
+            }
+            else
+            {
+                return View("EditPost");
+            }
+        }
+        public IActionResult DeleteConfirm(int id)
+        {
+            return View(FindPost(id));
+        }
+        public IActionResult Delete(int id)
+        {
+            posts.Remove(FindPost(id));
+            return View("List", posts);
+        }
+
+        public Post FindPost(int id)
+        {
+            foreach(var post in posts)
+            {
+                if (post.Id == id)
+                {
+                    return post;
+                }
+            }
+            throw new Exception("Taki Post Nie Istnieje");
+        }
+    }
+}
---
+++ ASPProjekt/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPProjekt.Models
+{
+    public class Post
+    {
+        [Required(ErrorMessage ="Musisz Podać Tytuł Posta")]
+        [MinLength(2)]
+        public string Title { get; set; }
+        public DateTime DateOfAdd { get; set; }
+
+        public int Id { get; set; }
+    }
+}
